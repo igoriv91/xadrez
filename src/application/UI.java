9087232d029d034d55201f84f9fe3330a043fa27
Igor Vitor <igorvitor.iv@gmail.com>
2021-11-10 22:40:59 -0300

    capiturando peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -66,9 +66,14 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turno: "+ chessMatch.getTurn());
-		System.out.println("Esperando o jogador: "+chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Esperando o jogador: "+chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}else {
+			System.out.println("CHECKMATE!");
+			System.out.println("GANHADOR: "+chessMatch.getCurrentPlayer()+" !");
 		}
 	}
 	
